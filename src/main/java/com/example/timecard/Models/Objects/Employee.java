1e31e67612f95c13b37a7883a98035bbbe127921
Mani9732 <mani.shah23@gmail.com>
2021-09-09 14:09:38 -0400

    some Functionality to Shift.java
    Signed-off-by: Mani9732 <mani.shah23@gmail.com>

--- src/main/java/com/example/timecard/Models/Objects/Employee.java
+++ src/main/java/com/example/timecard/Models/Objects/Employee.java
@@ -1,5 +1,56 @@
 package com.example.timecard.Models.Objects;
 
-public class Employee
+class Employee
 {
+	private final String firstName;
+	private final String lastName;
+	private final long empId;
+	private final String empPass;
+	private final double pay;
+
+	public Employee(String firstName, String lastName, long empId, String empPass, double pay)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.empId = empId;
+		this.empPass = empPass;
+		this.pay = pay;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public long getEmpId()
+	{
+		return empId;
+	}
+
+	public String getEmpPass()
+	{
+		return empPass;
+	}
+
+	public double getPay()
+	{
+		return pay;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Employee{" +
+				"firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", empId=" + empId +
+				", empPass='" + empPass + '\'' +
+				", pay=$" + pay +
+				'}';
+	}
 }
